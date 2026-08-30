df0218a184537ad09bfc9b0bc443a53e2d54ca80
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-08-30 21:52:17 +0530

    Create the entity classes.

---
+++ JpaAllRelationship/src/main/java/com/JpaAllRelationship/JpaAllRelationship/entity/Profile.java
@@ -0,0 +1,22 @@
+package com.JpaAllRelationship.JpaAllRelationship.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String bio;
+}
---
+++ JpaAllRelationship/src/main/java/com/JpaAllRelationship/JpaAllRelationship/entity/User.java
@@ -0,0 +1,27 @@
+package com.JpaAllRelationship.JpaAllRelationship.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
+
+}
