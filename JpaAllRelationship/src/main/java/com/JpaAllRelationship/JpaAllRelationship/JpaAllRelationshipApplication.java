908625f4b7f71dd4116ba23e404431d0c639568f
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-08-31 22:09:55 +0530

    -Main class

---
+++ JpaAllRelationship/src/main/java/com/JpaAllRelationship/JpaAllRelationship/JpaAllRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.JpaAllRelationship.JpaAllRelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAllRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAllRelationshipApplication.class, args);
+	}
+
+}
