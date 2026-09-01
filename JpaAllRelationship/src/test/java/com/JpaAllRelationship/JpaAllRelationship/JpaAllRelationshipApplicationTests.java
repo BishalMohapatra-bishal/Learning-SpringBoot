0bdc9e04a4d9773d35bbc3c45f53f84d682bc32b
BishalMohapatra-bishal <bishalmohapatra168@gmail.com>
2026-09-01 22:17:51 +0530

    All other files

---
+++ JpaAllRelationship/src/test/java/com/JpaAllRelationship/JpaAllRelationship/JpaAllRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JpaAllRelationship.JpaAllRelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAllRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
